b6fc348a17ff99ee75e5629205a1a2c3fd8c14e2
osvargin <osvargin@g,ail.com>
2024-10-18 16:13:10 +0300

    add new allure report

--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -15,11 +15,12 @@ avatar_ends = "-image.jpg"
 @allure.suite('Проверка запросов данных пользователей')
 @allure.title('Проверяем получение списка пользователей')
 def test_list_users():
+    url = base_url + list_users
     with allure.step(f'Делаем запрос по адресу {base_url + list_users}'):
-        url = base_url + list_users
-    response = httpx.get(url)
+        response = httpx.get(url)
     with allure.step('Проверяем код ответа'):
         assert response.status_code == 200
+
     data = response.json()["data"]
 
     for user in data:
@@ -31,17 +32,27 @@ def test_list_users():
                 assert user['avatar'].endswith(str(user['id']) + avatar_ends)
 
 
+@allure.suite('Проверка запроса одного пользователя')
+@allure.title('Проверяем получение одного пользователя по id')
 def test_single_user():
     url = base_url + single_user
-    response = httpx.get(url)
-    assert response.status_code == 200
+    with allure.step(f'Проверяем запрос по адресу: {base_url + single_user}'):
+        response = httpx.get(url)
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
+
     data = response.json()["data"]
-    assert data["email"].endswith(email_ends)
-    assert str(data["id"]) in data["avatar"]
-    assert data['avatar'].endswith(str(data['id']) + avatar_ends)
+    with allure.step('Проверяем окончание Email адреса = @reqres.in'):
+        assert data["email"].endswith(email_ends)
+    with allure.step('Проверяем наличие id в ссылке на аватарку'):
+        assert data['avatar'].endswith(str(data['id']) + avatar_ends)
 
 
+@allure.suite('Проверка запроса несуществующего пользователя')
+@allure.title('Проверяем получение пользователя по несуществующему id')
 def test_user_not_found():
     url = base_url + not_found
-    response = httpx.get(url)
-    assert response.status_code == 404
+    with allure.step(f'Проверяем запрос по адресу: {base_url + not_found}'):
+        response = httpx.get(url)
+    with allure.step(f'Проверяем, что в ответ будет получен код 404'):
+        assert response.status_code == 404
